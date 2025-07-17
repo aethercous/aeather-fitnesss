a7db5bcd1fa8ab9036f6ca8e37adf9a6a68628ad
aethercous <loop.co.us@gmail.com>
2025-07-17 11:54:10 -0400

    Initial Commit

---
+++ aeather-fitness/aeather_fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  aeather_fitnessApp.swift
+//  aeather-fitness
+//
+//  Created by Hudson Chuman on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct aeather_fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aeather-fitnessTests/aeather_fitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  aeather_fitnessTests.swift
+//  aeather-fitnessTests
+//
+//  Created by Hudson Chuman on 7/16/25.
+//
+
+import Testing
+@testable import aeather_fitness
+
+struct aeather_fitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
